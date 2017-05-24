b6f0b69c44d84a8d5db54491651827ca4b783aae
Max Stappenbelt <97043162@st.deltion.nl>
2017-05-24 09:03:01 +0200

    CHekkie 1

---
+++ CHekkie/CHekkie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CHekkie.Startup))]
+namespace CHekkie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
